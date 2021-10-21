3d7c0221a4be54f157e832fa0929b0ef332a23cc
sundeepbassi <sunnyeshe@yahoo.com>
2021-10-21 18:06:07 +0000

    Add code to bring_ship_place function

--- run.py
+++ run.py
@@ -11,17 +11,17 @@ from random import randint
 HIDDEN_BOARD = [[' '] * 8 for x in range(8)]
 GUESS_BOARD = [[' '] * 8 for x in range(8)]
 
-lets_to_nums = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7}
+lets_to_nums = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7}
 
 def print_board(board):
-    print(' a b c d f g h')
+    print(' A B C D E F G H')
     print(' -------------')
     row_number = 1
     for row in board:
         print("%d|%s|" % (row_number, "|".join(row)))
         row_number += 1
 
-def make_ships():
+def make_ships(board):
     for ship in range(5):
         ship_row, ship_column = randint(0, 7), randint(0, 7)
         while board[ship_row][ship_column] == 'x':
@@ -29,11 +29,18 @@ def make_ships():
         board[ship_row][ship_column] = 'X'
 
 def bring_ship_place():
-    pass
+    row = input('To hit a ship enter a row from 1-8')
+    while row not in '12345678':
+        print('Please enter a valid row')
+        row = input('To hit a ship enter a row from 1-8')
+    column = input('To hit a ship enter a column letter from A-H').upper()
+    while column not in 'ABCDEFGH':
+        Print('Please enter a valid column')
+        column = input('To hit a ship enter a column letter from A-H').upper()
+    return int(row) -1, lets_to_nums[column]
 
+    
 def count_attacked_ships():
-    pass
-
 create_ships()
 turns = 10
 while turns > 0:
