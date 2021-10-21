f55a090b2bd2ebc5f5ddd7c0d0d396b1e195ac19
sundeepbassi <sunnyeshe@yahoo.com>
2021-10-21 10:55:23 +0000

    Add function layout for the game

--- run.py
+++ run.py
@@ -1,12 +1,26 @@
 
 
 
-
+#create board layout 
 
 HIDDEN_BOARD = [[' '] * 8 for x in range(8)]
 GUESS_BOARD = [[' '] * 8 for x in range(8)]
 
-letters_to_numbers = 
+letters_to_numbers = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7}
+
+def board():
+    pass
+
+def make_ships():
+
+
+def bring_ship_place():
+    pass
+
+def count_attacked_ships():
+    pass
+
+
 
 
 
