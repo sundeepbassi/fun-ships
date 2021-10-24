2e7c9a3ac8de2227bf31029efe9339499628d147
sundeepbassi <sunnyeshe@yahoo.com>
2021-10-24 03:56:51 +0000

    Add code in place_ships function

--- run.py
+++ run.py
@@ -18,12 +18,28 @@ def print_board(board):
         
 
 def place_ships(board):
-    pass
+    # loop through length of ships
+    for ship_length in LENGTH_OF_SHIPS:
+        # loop until ship fits and doesn't overlap
+        while True:
+            if board == COMPUTER_BOARD:
+                orientation, row, column = random.choice(["H", "V"]), 
+                random.randint(0, 7), random.randint(0, 7)
+                if check_ship_fit(ship_length, row, column, orientation):
+                    
 
+def check_ship_fit(SHIP_LENGTH, row, column, orientation):
+    if orientation == "H":
+        if column + SHIP_LENGTH > 8:
+            return False
+        else:
+            return True
+    else:
+        if row + SHIP_LENGTH > 8:
+            return False
+        else:
+            return True
 
-def check_ship_fit():
-    pass
-    
 
 def ship_overlaps():
     pass
