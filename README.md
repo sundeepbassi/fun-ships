3e60f2a4ef09961f8544fc324be487251f801e49
sundeepbassi <sunnyeshe@yahoo.com>
2021-10-24 18:50:58 +0000

    Conduct validator check on pep8

--- README.md
+++ README.md
@@ -109,6 +109,12 @@ NEED TO UPDATE THIS ONCE THE COMPUTER GRID BOARD IS ALSO PLACED IN THE GAME.
 
 - [Knowledge Mavens](https://www.youtube.com/watch?v=tF1WRCrd_HQ&t=66s) Youtube Tutorial.
 
+- The code for the print_board function is accredited to:
+
+- [Knowledge Mavens](https://www.youtube.com/watch?v=tF1WRCrd_HQ&t=66s) Youtube Tutorial.
+
+
+
 
 
 
--- run.py
+++ run.py
@@ -9,21 +9,22 @@ board = [' ' for i in range(10)]
 
 # print the board
 def print_board():
-  print('#########################')
-  print(board[1] + '|' + board[2] + '|' + board[3])
-  print('----------')
-  print(board[4] + '|' + board[5] + '|' + board[6])
-  print('----------')
-  print(board[7] + '|' + board[8] + '|' + board[9])
+    print('#########################')
+    print(board[1] + '|' + board[2] + '|' + board[3])
+    print('----------')
+    print(board[4] + '|' + board[5] + '|' + board[6])
+    print('----------')
+    print(board[7] + '|' + board[8] + '|' + board[9])
 
 
 # is the board full
 def is_full():
-  return ' ' not in board[1:10]
+    return ' ' not in board[1:10]
  
+
 # check if there is a winner
 def check_win(board, letter):
-  return (board[1] == board[2] == board[3] == letter) or \
+    return (board[1] == board[2] == board[3] == letter) or \
          (board[4] == board[5] == board[6] == letter) or \
          (board[7] == board[8] == board[9] == letter) or \
          (board[1] == board[4] == board[7] == letter) or \
