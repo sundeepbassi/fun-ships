df5f04d1b95070188e344340fbab2bc0b538977e
sundeepbassi <sunnyeshe@yahoo.com>
2022-02-12 15:28:55 +0000

    Add and rectify code to stop invalid data entry

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 import random
-
+import re
 # The code for this file is accredited to:
 # Knowledge Mavens2(https://www.youtube.com/watch?v=oe0kIt3kE2g&t=3s) Youtube Tutorial.  # noqa: E501
 
@@ -14,6 +14,7 @@ board = [' ' for i in range(10)]
 
 # This function will add a print of the grid board
 def print_board():
+    """ print board docstring """
     print('#########################')
     print(board[1] + '|' + board[2] + '|' + board[3])
     print('----------')
@@ -24,34 +25,39 @@ def print_board():
 
 # This function will check if the  board is full
 def is_full():
+    """ is full docstring """
     return ' ' not in board[1:10]
 
 
 # check if there is a winner
 def check_win(board, letter):
+    """ check win docstring """
     return (board[1] == board[2] == board[3] == letter) or \
-         (board[4] == board[5] == board[6] == letter) or \
-         (board[7] == board[8] == board[9] == letter) or \
-         (board[1] == board[4] == board[7] == letter) or \
-         (board[2] == board[5] == board[8] == letter) or \
-         (board[3] == board[6] == board[9] == letter) or \
-         (board[1] == board[5] == board[9] == letter) or \
-         (board[3] == board[5] == board[7] == letter) or \
-         (board[1] == board[2] == board[3] == letter)
+        (board[4] == board[5] == board[6] == letter) or \
+        (board[7] == board[8] == board[9] == letter) or \
+        (board[1] == board[4] == board[7] == letter) or \
+        (board[2] == board[5] == board[8] == letter) or \
+        (board[3] == board[6] == board[9] == letter) or \
+        (board[1] == board[5] == board[9] == letter) or \
+        (board[3] == board[5] == board[7] == letter) or \
+        (board[1] == board[2] == board[3] == letter)
 
 
 # This function will check for a free space
 def is_free(board, position):
+    """ is free docstring """
     return board[position] == ' '
 
 
 # This function will place the marker on the board
 def place_marker(board, mark, position):
+    """ place marker docstring """
     board[position] = mark
 
 
 # who goes first
 def who_goes_first():
+    """ who goes first docstring """
     if random.randint(0, 1) == 0:
         return playerLetter
     else:
@@ -60,11 +66,17 @@ def who_goes_first():
 
 # player turn
 def player_turn():
+    """ player turn docstring """
     while True:
         try:
-            position = int(input('Choose a number between 1 & 9: '))
+            position = input('Choose a number between 1 & 9: ')
+            if re.match(r'^\d+$', str(position)):
+                position = int(position)
+            else:
+                print('Please choose a number between 1 and 9, have another go')  # noqa: E501
+                continue
             if position < 1 or position > 9:
-                print('Please choose a number between 1 and 9, have another go')
+                print('Please choose a number between 1 and 9, have another go')  # noqa: E501
             else:
                 if is_free(board, position):
                     place_marker(board, playerLetter, position)
@@ -73,10 +85,11 @@ def player_turn():
                     print('This position is not free, please try again')
         except ValueError:
             print("not a valid input")
-    
-    
+
+
 # create a copy of the board
 def duplicate_board(board):
+    """ duplicate board docstring """
     duplicate_board = []
     for i in board:
         duplicate_board.append(i)
@@ -85,6 +98,7 @@ def duplicate_board(board):
 
 # computer turn
 def computer_turn():
+    """ computer turn docstring """
     for i in range(1, 10):
         copy = duplicate_board(board)
         if is_free(copy, i):
@@ -120,6 +134,7 @@ def computer_turn():
 
 # run the game
 def main():
+    """ run the game docstring """
     print('Welcome to Noughts & Crosses (Tic Tac Toe!)')
     turn = who_goes_first()
     while True:
